efa7ab1f70e31307ff0d7067be8e25e7bee222da
= <=>
2021-09-30 10:28:05 -0500

    Added validation to questions

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHut Username'
+            message: 'Enter your GitHut Username',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }else {
+                    console.log("Please enter your GitHub name!");
+                    return false;
+                }
+            }
         },
         {
             type:'input',
@@ -50,7 +66,15 @@ const promptProject = portfolioData => {
         {
             type:'input',
             name:'description',
-            message:'Provide a description of the project(Required)'
+            message:'Provide a description of the project(Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                }else {
+                    console.log("Please enter a description for your project!");
+                    return false;
+                }
+            }
         },
         {
             type:'checkbox',
@@ -61,7 +85,15 @@ const promptProject = portfolioData => {
         {
             type:'input',
             name:'link',
-            message:'Enter the GitHub link to your project (Required)'
+            message:'Enter the GitHub link to your project (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                }else {
+                    console.log('Please enter a GitHub link!');
+                    return false;
+                }
+            }
         },
         {
             type:'confirm',
